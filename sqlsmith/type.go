8260028fce79860a3823b7ea93a7d7f25a704c26
Matt Jibson <matt.jibson@gmail.com>
2019-01-26 00:09:28 -0500

    support more types

--- sqlsmith/type.go
+++ sqlsmith/type.go
@@ -9,14 +9,23 @@ import (
 )
 
 var typeNames = func() map[string]types.T {
-	m := map[string]types.T{}
+	m := map[string]types.T{
+		"int8":   types.Int,
+		"int8[]": types.TArray{types.Int},
+		"float8": types.Float,
+	}
 	for _, T := range types.OidToType {
 		m[T.SQLName()] = T
+		m[T.String()] = T
 	}
 	return m
 }()
 
 func typeFromName(name string) types.T {
+	// Fill in any collated string type names we see.
+	if sp := strings.Split(name, "STRING COLLATE "); len(sp) == 2 {
+		typeNames[strings.ToLower(name)] = types.TCollatedString{sp[1]}
+	}
 	typ, ok := typeNames[strings.ToLower(name)]
 	if !ok {
 		panic(fmt.Errorf("unknown type name: %s", name))
